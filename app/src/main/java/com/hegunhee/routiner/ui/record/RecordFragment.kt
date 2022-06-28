57793b7ef90ab6153788c48c0897a7de751a9028
hegunhee <gunhee0072@gmail.com>
2022-06-28 15:48:25 +0900

    RecordFragment Xml그리기 및 메뉴 장착

--- app/src/main/java/com/hegunhee/routiner/ui/record/RecordFragment.kt
+++ app/src/main/java/com/hegunhee/routiner/ui/record/RecordFragment.kt
@@ -1,7 +1,11 @@
 package com.hegunhee.routiner.ui.record
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import com.hegunhee.routiner.R
 import com.hegunhee.routiner.databinding.FragmentRecordBinding
 import com.hegunhee.routiner.ui.BaseFragment
@@ -14,5 +18,25 @@ class RecordFragment : BaseFragment<FragmentRecordBinding>(R.layout.fragment_rec
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (requireActivity() as MainActivity).supportActionBar?.title = "record"
+        setHasOptionsMenu(true)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.record_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.click_left -> {
+                Toast.makeText(requireContext(), "click_left", Toast.LENGTH_SHORT).show()
+            }
+            R.id.click_right -> {
+                Toast.makeText(requireContext(), "click_right", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
