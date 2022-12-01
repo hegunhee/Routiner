83ee564a7e3d53ce8ba71083de21af60c57aaeb3
hegunhee <gunhee0072@gmail.com>
2022-12-01 15:13:56 +0900

    [Divide-InsertRoutineDialog] Dialog 크기 설정

--- feature/src/main/java/com/hegunhee/feature/base/BaseDialog.kt
+++ feature/src/main/java/com/hegunhee/feature/base/BaseDialog.kt
@@ -1,9 +1,13 @@
 package com.hegunhee.feature.base
 
+import android.content.Context
+import android.graphics.Point
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -25,8 +29,43 @@ abstract class BaseDialog<T : ViewDataBinding>(@LayoutRes private val layoutResI
         return binding.root
     }
 
+    override fun onResume() {
+        super.onResume()
+        context?.resizeDialog()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun Context.resizeDialog() {
+        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        if(Build.VERSION.SDK_INT < 30){
+            val display = windowManager.defaultDisplay
+            val size = Point()
+
+            display.getSize(size)
+
+            val window = this@BaseDialog.dialog?.window
+
+            val x = (size.x * WIDTH_PERCENT).toInt()
+            val y = (size.y * HEIGHT_PERCENT).toInt()
+            window?.setLayout(x,y)
+        }else{
+            val rect = windowManager.currentWindowMetrics.bounds
+
+            val window = this@BaseDialog.dialog?.window
+
+            val x = (rect.width() * WIDTH_PERCENT).toInt()
+            val y = (rect.height() * HEIGHT_PERCENT).toInt()
+
+            window?.setLayout(x,y)
+        }
+    }
+
+    companion object {
+        private const val WIDTH_PERCENT = 0.8
+        private const val HEIGHT_PERCENT = 0.3
     }
 }
