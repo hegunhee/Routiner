fe11476e816d0239ecfcaa89448d38602cdebfa0
hegunhee <gunhee0072@gmail.com>
2022-12-03 00:20:37 +0900

    [category-dialog] InsertCategoryDialogFragment, ViewModel 추가

---
+++ feature/src/main/java/com/hegunhee/feature/category/insert/InsertCategoryDialogFragment.kt
@@ -0,0 +1,10 @@
+package com.hegunhee.feature.category.insert
+
+import com.hegunhee.feature.R
+import com.hegunhee.feature.base.BaseDialog
+import com.hegunhee.feature.databinding.DialogInsertCategoryBinding
+
+class InsertCategoryDialogFragment : BaseDialog<DialogInsertCategoryBinding>(R.layout.dialog_insert_category, widthPercent = 0.6, heightPercent = 0.2) {
+
+
+}
---
+++ feature/src/main/java/com/hegunhee/feature/category/insert/InsertCategoryDialogViewModel.kt
@@ -0,0 +1,6 @@
+package com.hegunhee.feature.category.insert
+
+import androidx.lifecycle.ViewModel
+
+class InsertCategoryDialogViewModel : ViewModel() {
+}
