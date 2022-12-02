8d160a561e94ba1f572566a255be2f3f94ba34ec
hegunhee <gunhee0072@gmail.com>
2022-12-02 18:25:15 +0900

    [custom-plugin] app 모듈 plugin 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,5 @@
 plugins {
     id("android.application")
-//    id("com.android.application")
-//    id("org.jetbrains.kotlin.android")
-//    id("dagger.hilt.android.plugin")
-    id("kotlin-kapt")
 }
 
 android {
@@ -12,7 +8,6 @@ android {
         applicationId = "com.hegunhee.routiner"
         versionCode = (3)
         versionName = "1.2"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -24,29 +19,4 @@ android {
 }
 dependencies {
 
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.constraintlayout)
-    implementation(libs.junit)
-    androidTestImplementation(libs.testext)
-    androidTestImplementation(libs.espresso)
-
-    implementation(libs.bundles.navigation)
-
-    implementation(libs.activity.ktx)
-    implementation(libs.fragment.ktx)
-
-    implementation(libs.lifecycle.livedata)
-    implementation(libs.lifecycle.scope)
-
-    //Room DB
-    implementation(libs.bundles.room)
-    kapt(libs.room.compiler)
-
-    //Hilt
-    implementation(libs.bundles.hilt)
-    kapt(libs.hilt.compiler)
-
-    implementation(libs.gson)
 }
