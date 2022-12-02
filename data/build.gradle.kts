391fd8f43e5711ed6c6459685b5698dff7bca70d
hegunhee <gunhee0072@gmail.com>
2022-12-02 18:13:20 +0900

    [custom-plugin] data module plugin교체

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,21 +1,10 @@
 
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-    id("dagger.hilt.android.plugin")
+    id("android.library.convention")
+    id("android.hilt")
 }
 
 android {
-    compileSdk = 32
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 32
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
 
     buildTypes {
         release {
@@ -26,23 +15,15 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
 
     implementation(project(":domain"))
-
-    //Room DB
+    
     implementation(libs.bundles.room)
     kapt(libs.room.compiler)
-    
+
     implementation(libs.gson)
 
 }
