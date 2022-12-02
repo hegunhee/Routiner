3b55482101b621524e20d36c9518223df1d3a938
hegunhee <gunhee0072@gmail.com>
2022-12-02 17:42:24 +0900

    [custom-plugin]  hilt 커스텀 플러그인 추가

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -11,3 +11,15 @@ dependencies{
     compileOnly(libs.android.gradlePlugin)
     compileOnly(libs.kotlin.gradlePlugin)
 }
+gradlePlugin{
+    plugins{
+        register("androidLibrary"){
+            id = "android.library.convention"
+            implementationClass = "com.example.convention.AndroidLibraryConventionPlugin"
+        }
+        register("androidHilt"){
+            id = "android.hilt"
+            implementationClass = "com.example.convention.AndroidHiltConventionPlugin"
+        }
+    }
+}
