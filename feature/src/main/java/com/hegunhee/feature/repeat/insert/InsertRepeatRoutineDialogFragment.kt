46c3e8ca25e4fa7a71206c83299cf4a8e76d141c
hegunhee <gunhee0072@gmail.com>
2022-12-03 22:02:11 +0900

    [repeat-routine-dialog] 카테고리 추가 기능 구현

--- feature/src/main/java/com/hegunhee/feature/repeat/insert/InsertRepeatRoutineDialogFragment.kt
+++ feature/src/main/java/com/hegunhee/feature/repeat/insert/InsertRepeatRoutineDialogFragment.kt
@@ -2,14 +2,17 @@ package com.hegunhee.feature.repeat.insert
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isEmpty
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.chip.Chip
 import com.hegunhee.feature.R
 import com.hegunhee.feature.base.BaseDialog
 import com.hegunhee.feature.category.insert.InsertCategoryDialogFragment
 import com.hegunhee.feature.databinding.DialogRepeatRoutineBinding
+import com.hegunhee.feature.util.addCheckableChip
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -23,21 +26,45 @@ class InsertRepeatRoutineDialogFragment : BaseDialog<DialogRepeatRoutineBinding>
         super.onViewCreated(view, savedInstanceState)
         binding.apply { viewModel = this@InsertRepeatRoutineDialogFragment.viewModel }
         observeData()
+        categoryGroupClickListener()
     }
 
     private fun observeData(){
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.RESUMED){
-                viewModel.navigationActions.collect{
-                    when(it){
-                        InsertRepeatRoutineNavigationAction.DisMissDialog -> {
-                            dismiss()
-                        }
-                        InsertRepeatRoutineNavigationAction.OpenInsertCategoryDialog -> {
-                            InsertCategoryDialogFragment().show(childFragmentManager,"insert_category")
+                launch {
+                    viewModel.navigationActions.collect{
+                        when(it){
+                            InsertRepeatRoutineNavigationAction.DisMissDialog -> {
+                                dismiss()
+                            }
+                            InsertRepeatRoutineNavigationAction.OpenInsertCategoryDialog -> {
+                                InsertCategoryDialogFragment().show(childFragmentManager,"insert_category")
+                            }
                         }
                     }
                 }
+                launch {
+                    viewModel.categoryList.collect{
+                        if (it.isEmpty()) return@collect
+                        if (binding.categoryGroup.isEmpty()) {
+                            it.forEach {
+                                binding.categoryGroup.addCheckableChip(it.name)
+                            }
+                        } else {
+                            binding.categoryGroup.addCheckableChip(it.last().name)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private fun categoryGroupClickListener(){
+        binding.categoryGroup.setOnCheckedStateChangeListener { group, checkedIdList ->
+            viewModel.categoryText = if(checkedIdList.isEmpty()){
+                ""
+            }else{
+                group.findViewById<Chip>(checkedIdList.first()).text.toString()
             }
         }
     }
--- feature/src/main/java/com/hegunhee/feature/repeat/insert/InsertRepeatRoutineDialogViewModel.kt
+++ feature/src/main/java/com/hegunhee/feature/repeat/insert/InsertRepeatRoutineDialogViewModel.kt
@@ -3,18 +3,25 @@ package com.hegunhee.feature.repeat.insert
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.domain.model.Category
+import com.example.domain.usecase.category.GetAllCategoryListByFlowUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class InsertRepeatRoutineDialogViewModel @Inject constructor() : ViewModel() , InsertRepeatRoutineActionHandler{
+class InsertRepeatRoutineDialogViewModel @Inject constructor(
+    private val allCategoryListByFlowUseCase: GetAllCategoryListByFlowUseCase
+) : ViewModel() , InsertRepeatRoutineActionHandler{
 
     val repeatRoutineText : MutableStateFlow<String> = MutableStateFlow<String>("")
+
+    var categoryText : String = ""
+
+    private val _categoryList : Flow<List<Category>> = allCategoryListByFlowUseCase()
+    val categoryList : Flow<List<Category>>
+    get() = _categoryList
 
     private val _navigationActions : MutableSharedFlow<InsertRepeatRoutineNavigationAction> = MutableSharedFlow<InsertRepeatRoutineNavigationAction>()
     val navigationActions : SharedFlow<InsertRepeatRoutineNavigationAction> = _navigationActions.asSharedFlow()
@@ -26,6 +33,8 @@ class InsertRepeatRoutineDialogViewModel @Inject constructor() : ViewModel() , I
     }
 
     override fun successRepeatRoutine() {
+        println(repeatRoutineText)
+        println(categoryText)
     }
 
     override fun openInsertCategoryDialog() {
