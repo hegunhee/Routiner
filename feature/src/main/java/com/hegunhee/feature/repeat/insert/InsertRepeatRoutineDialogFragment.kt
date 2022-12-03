df73729b3a2ed8a5e8e5f7f30209266a5237106c
hegunhee <gunhee0072@gmail.com>
2022-12-03 21:35:02 +0900

    [repeat-routine-dialog] RepeatRoutineDialog 추가

---
+++ feature/src/main/java/com/hegunhee/feature/repeat/insert/InsertRepeatRoutineDialogFragment.kt
@@ -0,0 +1,12 @@
+package com.hegunhee.feature.repeat.insert
+
+import com.hegunhee.feature.R
+import com.hegunhee.feature.base.BaseDialog
+import com.hegunhee.feature.databinding.DialogRepeatRoutineBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class InsertRepeatRoutineDialogFragment : BaseDialog<DialogRepeatRoutineBinding>(R.layout.dialog_repeat_routine, widthPercent = 0.8, heightPercent = 0.4){
+
+
+}
---
+++ feature/src/main/java/com/hegunhee/feature/repeat/insert/InsertRepeatRoutineDialogViewModel.kt
@@ -0,0 +1,9 @@
+package com.hegunhee.feature.repeat.insert
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class InsertRepeatRoutineDialogViewModel @Inject constructor() : ViewModel() {
+}
