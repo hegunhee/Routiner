9ddefb8e2567f1b302684d8b2d735ab0d541e05e
hegunhee <gunhee0072@gmail.com>
2022-12-15 16:56:39 +0900

    [micro-module] util도 common 모듈로 옮김

--- common/src/main/java/com/example/common/util/ChipUtil.kt
+++ common/src/main/java/com/example/common/util/ChipUtil.kt
@@ -2,9 +2,9 @@ package com.hegunhee.feature.util
 
 import android.content.res.ColorStateList
 import androidx.core.content.ContextCompat
+import com.example.common.R
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
-import com.hegunhee.feature.R
 
 fun Chip.setRoutineDefaultColor() {
     chipBackgroundColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.teal_200))
