8bd26f816002ed5ee137f830b67f83ab210c3599
hegunhee <gunhee0072@gmail.com>
2023-05-27 11:27:50 +0900

    [refactoring-data-module] feat : Hilt 의존성 주입 설정과 사용해야되는 Dao 객체 주입

--- data/src/main/java/com/example/data/dataSource/local/DefaultLocalDataSource.kt
+++ data/src/main/java/com/example/data/dataSource/local/DefaultLocalDataSource.kt
@@ -2,7 +2,13 @@ package com.example.data.dataSource.local
 
 import javax.inject.Inject
 
+@Singleton
 class DefaultLocalDataSource @Inject constructor(
-
+    private val routineDao: RoutineDao,
+    private val dateDao: DateDao,
+    private val reviewDao: ReviewDao,
+    private val repeatRoutineDao: RepeatRoutineDao,
+    private val categoryDao: CategoryDao,
+    private val sharedPreferenceManager: SharedPreferenceManager
 ) : LocalDataSource {
 }
