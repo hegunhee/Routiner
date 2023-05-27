255504fe73917e0e2d853d4d2ae1bc7298bc76df
hegunhee <gunhee0072@gmail.com>
2023-05-27 11:35:18 +0900

    [refactoring-data-module] fix : dataSource에서 사용하는 데이터로 매개변수, 리턴값 변경

--- data/src/main/java/com/example/data/dataSource/local/LocalDataSource.kt
+++ data/src/main/java/com/example/data/dataSource/local/LocalDataSource.kt
@@ -1,43 +1,44 @@
 package com.example.data.dataSource.local
 
+import com.example.data.entity.*
 import com.example.domain.model.*
 import kotlinx.coroutines.flow.Flow
 
 interface LocalDataSource {
 
-    fun getAllDailyRoutineByFlow(date : Int) : Flow<List<Routine>>
+    fun getAllDailyRoutineByFlow(date : Int) : Flow<List<RoutineEntity>>
 
-    suspend fun insertRoutine(routine: Routine)
+    suspend fun insertRoutine(routineEntity: RoutineEntity)
 
     suspend fun deleteAllRoutineByDate(date : Int)
 
     suspend fun deleteRoutine(id : Int)
 
-    suspend fun getRoutineListByDate(date :Int) : List<Routine>
+    suspend fun getRoutineListByDate(date :Int) : List<RoutineEntity>
 
-    suspend fun insertDate(date : Date)
+    suspend fun insertDate(date : DateEntity)
 
-    suspend fun getAllDateList() : List<Date>
+    suspend fun getAllDateList() : List<DateEntity>
 
-    suspend fun getReviewListByDate(date : Int) : List<Review>
+    suspend fun getReviewListByDate(date : Int) : List<ReviewEntity>
 
-    suspend fun insertReview(review : Review)
+    suspend fun insertReview(review : ReviewEntity)
 
-    suspend fun deleteReview(review: Review)
+    suspend fun deleteReview(review: ReviewEntity)
 
-    suspend fun insertRepeatRoutine(repeatRoutine: RepeatRoutine)
+    suspend fun insertRepeatRoutine(repeatRoutine: RepeatRoutineEntity)
 
-    fun getAllRepeatRoutineListByFlow() : Flow<List<RepeatRoutine>>
+    fun getAllRepeatRoutineListByFlow() : Flow<List<RepeatRoutineEntity>>
 
-    suspend fun getAllRepeatRoutineList() : List<RepeatRoutine>
+    suspend fun getAllRepeatRoutineList() : List<RepeatRoutineEntity>
 
     suspend fun deleteRepeatRoutine(text: String)
 
-    suspend fun insertAllRoutine(routineList: List<Routine>)
+    suspend fun insertAllRoutine(routineList: List<RoutineEntity>)
 
-    suspend fun insertCategory(category: Category)
+    suspend fun insertCategory(category: CategoryEntity)
 
-    fun getAllCategoryListByFlow() : Flow<List<Category>>
+    fun getAllCategoryListByFlow() : Flow<List<CategoryEntity>>
 
     suspend fun getCurrentDate() : Int
 
