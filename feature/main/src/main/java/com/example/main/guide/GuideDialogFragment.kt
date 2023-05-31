41aee1884809bae8bfab3ef1d02a06d18b07755b
hegunhee <gunhee0072@gmail.com>
2023-05-31 14:16:41 +0900

    [guide-dialogFragment] test : GuideDialog Test 코드 작성

--- feature/main/src/main/java/com/example/main/guide/GuideDialogFragment.kt
+++ feature/main/src/main/java/com/example/main/guide/GuideDialogFragment.kt
@@ -1,11 +1,30 @@
 package com.example.main.guide
 
+import android.os.Bundle
+import android.view.View
 import com.example.main.R
 import com.example.main.databinding.DialogGuideBinding
 import com.hegunhee.common.base.BaseDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GuideDialogFragment : BaseDialog<DialogGuideBinding>(layoutResId = R.layout.dialog_guide) {
+class GuideDialogFragment() : BaseDialog<DialogGuideBinding>(layoutResId = R.layout.dialog_guide,widthPercent = 0.8,heightPercent = 0.4) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.alertAcceptButton.setOnClickListener {
+            dismissAllowingStateLoss()
+        }
+    }
+
+
+
+    companion object{
+        const val TAG = "GuideDialogFragment"
+        fun getInstance() : GuideDialogFragment{
+            return GuideDialogFragment().apply {
+                isCancelable = false
+            }
+        }
+    }
 }
