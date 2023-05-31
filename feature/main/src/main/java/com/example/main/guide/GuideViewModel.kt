a9f40ef3657ec17d5fa488f5f6af7d5b8df08a58
hegunhee <gunhee0072@gmail.com>
2023-05-31 14:19:31 +0900

    [guide- dialogFragment] feat : GuideViewModel 생성

---
+++ feature/main/src/main/java/com/example/main/guide/GuideViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.main.guide
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GuideViewModel @Inject constructor(): ViewModel() {
+}
