91f9827a1ad285d7a98cc11e66338b93dcf8f5c4
hegunhee <gunhee0072@gmail.com>
2023-05-31 14:49:09 +0900

    [guide-dialogFragment] feat : viewModel에서 옵저빙한 데이터로 dialog 종료 설정

--- feature/main/src/main/java/com/example/main/guide/GuideDialogFragment.kt
+++ feature/main/src/main/java/com/example/main/guide/GuideDialogFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.main.R
 import com.example.main.databinding.DialogGuideBinding
 import com.hegunhee.common.base.BaseDialog
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class GuideDialogFragment() : BaseDialog<DialogGuideBinding>(layoutResId = R.layout.dialog_guide,widthPercent = 0.8,heightPercent = 0.4) {
@@ -18,11 +21,19 @@ class GuideDialogFragment() : BaseDialog<DialogGuideBinding>(layoutResId = R.lay
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
-//        binding.alertAcceptButton.setOnClickListener {
-//            dismissAllowingStateLoss()
-//        }
+        observeData()
     }
 
+    private fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            launch {
+                viewModel.navigateDismissDialog.collect{
+                    Toast.makeText(requireActivity(), getString(R.string.notification_setting,if(viewModel.isAllowNotification.value) "승인" else "해제"), Toast.LENGTH_SHORT).show()
+                    dismissAllowingStateLoss()
+                }
+            }
+        }
+    }
 
 
     companion object{
