b56859d32e96987decc69c753cc7a9b3a6858cba
hegunhee <gunhee0072@gmail.com>
2023-05-31 14:53:02 +0900

    [guide-dialogFragment] refactor : 사용하지않는 import 코드 삭제

--- feature/main/src/main/java/com/example/main/guide/GuideDialogFragment.kt
+++ feature/main/src/main/java/com/example/main/guide/GuideDialogFragment.kt
@@ -1,9 +1,7 @@
 package com.example.main.guide
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
--- feature/main/src/main/java/com/example/main/guide/GuideViewModel.kt
+++ feature/main/src/main/java/com/example/main/guide/GuideViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.main.guide
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.usecase.date.SetAppFirstOpenedUseCase
