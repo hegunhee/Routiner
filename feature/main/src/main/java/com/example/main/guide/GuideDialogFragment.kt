5e15799b26c71275e91b3a20c9ae068e094521df
hegunhee <gunhee0072@gmail.com>
2023-05-31 14:35:54 +0900

    [guide-dialogFragment] feat : fragment에 viewModel 설정 및 xml 연동

--- feature/main/src/main/java/com/example/main/guide/GuideDialogFragment.kt
+++ feature/main/src/main/java/com/example/main/guide/GuideDialogFragment.kt
@@ -1,7 +1,10 @@
 package com.example.main.guide
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.main.R
 import com.example.main.databinding.DialogGuideBinding
 import com.hegunhee.common.base.BaseDialog
@@ -10,11 +13,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class GuideDialogFragment() : BaseDialog<DialogGuideBinding>(layoutResId = R.layout.dialog_guide,widthPercent = 0.8,heightPercent = 0.4) {
 
+    private val viewModel : GuideViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.alertAcceptButton.setOnClickListener {
-            dismissAllowingStateLoss()
-        }
+        binding.viewModel = viewModel
+//        binding.alertAcceptButton.setOnClickListener {
+//            dismissAllowingStateLoss()
+//        }
     }
 
 
