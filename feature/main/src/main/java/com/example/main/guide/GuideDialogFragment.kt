520b6e3f59ed54da9b18ac2d17380d33d88ad499
hegunhee <gunhee0072@gmail.com>
2023-05-31 13:50:08 +0900

    [guide-dialogFragment] feat : GuideDialogFragment 생성 및 기본 Hilt 설정

---
+++ feature/main/src/main/java/com/example/main/guide/GuideDialogFragment.kt
@@ -0,0 +1,11 @@
+package com.example.main.guide
+
+import com.example.main.R
+import com.example.main.databinding.DialogGuideBinding
+import com.hegunhee.common.base.BaseDialog
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class GuideDialogFragment : BaseDialog<DialogGuideBinding>(layoutResId = R.layout.dialog_guide) {
+
+}
