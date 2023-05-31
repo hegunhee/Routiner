2872c2e4c6c695203966721625d73bf25f29a0d3
hegunhee <gunhee0072@gmail.com>
2023-05-31 14:36:21 +0900

    [guide-dialogFragment] test : switch check dataBinding 연동 테스트

--- feature/main/src/main/java/com/example/main/guide/GuideViewModel.kt
+++ feature/main/src/main/java/com/example/main/guide/GuideViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.main.guide
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class GuideViewModel @Inject constructor(): ViewModel() {
+
+    val isAllowNotification : MutableStateFlow<Boolean> = MutableStateFlow<Boolean>(false)
+
+
+    fun onClickDismissDialogButton() {
+        Log.d("TEST!!!",isAllowNotification.value.toString())
+    }
 }
