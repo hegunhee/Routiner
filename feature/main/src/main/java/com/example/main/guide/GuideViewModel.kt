ed0add00ff36c8f0c421a1af1d0d5d76a3412d67
hegunhee <gunhee0072@gmail.com>
2023-05-31 14:44:50 +0900

    [guide-dialogFragment] feat : dialog가 종료될때 noti 허용정보와 종료 이벤트 emit

--- feature/main/src/main/java/com/example/main/guide/GuideViewModel.kt
+++ feature/main/src/main/java/com/example/main/guide/GuideViewModel.kt
@@ -2,19 +2,31 @@ package com.example.main.guide
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.usecase.notification.SetNotiSendValueUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class GuideViewModel @Inject constructor(): ViewModel() {
+class GuideViewModel @Inject constructor(
+    private val setNotiSendValueUseCase : SetNotiSendValueUseCase
+): ViewModel() {
 
     val isAllowNotification : MutableStateFlow<Boolean> = MutableStateFlow<Boolean>(false)
 
     private val _navigateDismissDialog : MutableSharedFlow<Unit> = MutableSharedFlow()
     val navigateDismissDialog : SharedFlow<Unit> = _navigateDismissDialog.asSharedFlow()
 
+
     fun onClickDismissDialogButton() {
-        Log.d("TEST!!!",isAllowNotification.value.toString())
+        viewModelScope.launch {
+            setNotiSendValueUseCase(isAllowNotification.value)
+            _navigateDismissDialog.emit(Unit)
+        }
     }
 }
