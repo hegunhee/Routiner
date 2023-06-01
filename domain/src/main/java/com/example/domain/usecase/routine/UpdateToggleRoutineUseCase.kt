9b527effd833e8c7762dc77c003cc830758feed3
hegunhee <gunhee0072@gmail.com>
2023-06-01 09:56:04 +0900

    [refactor-daily] feat : 토글된 루틴 업데이트하는 UseCase 작성

---
+++ domain/src/main/java/com/example/domain/usecase/routine/UpdateToggleRoutineUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.domain.usecase.routine
+
+import com.example.domain.model.Routine
+import com.example.domain.repository.Repository
+import javax.inject.Inject
+
+class UpdateToggleRoutineUseCase @Inject constructor(private val repository: Repository) {
+
+    suspend operator fun invoke(routine : Routine) {
+        repository.updateRoutine(routine)
+    }
+}
