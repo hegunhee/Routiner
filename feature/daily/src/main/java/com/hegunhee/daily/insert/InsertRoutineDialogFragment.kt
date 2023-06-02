92efd33725c1f71f8b12a9f8e30af81852a31758
hegunhee <gunhee0072@gmail.com>
2023-06-02 14:51:31 +0900

    [category-recyclerView] feat : fragment에서 chip관련 코드 걷어내기

--- feature/daily/src/main/java/com/hegunhee/daily/insert/InsertRoutineDialogFragment.kt
+++ feature/daily/src/main/java/com/hegunhee/daily/insert/InsertRoutineDialogFragment.kt
@@ -3,18 +3,15 @@ package com.hegunhee.daily.insert
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.core.view.isEmpty
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.google.android.material.chip.Chip
 import com.hegunhee.category.CategoryAdapter
 import com.hegunhee.common.base.BaseDialog
 import com.hegunhee.category.insert.InsertCategoryDialogFragment
 import com.hegunhee.daily.R
 import com.hegunhee.daily.databinding.DialogDailyRoutineBinding
-import com.hegunhee.common.util.addCheckableChip
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -22,14 +19,16 @@ import kotlinx.coroutines.launch
 class InsertRoutineDialogFragment : BaseDialog<DialogDailyRoutineBinding>(R.layout.dialog_daily_routine, widthPercent = 0.8, heightPercent = 0.4){
 
     private val viewModel : InsertRoutineDialogViewModel by viewModels()
+    private lateinit var categoryAdapter : CategoryAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        categoryAdapter = CategoryAdapter(viewModel)
         binding.apply {
             viewModel = this@InsertRoutineDialogFragment.viewModel
+            categoryRecyclerView.adapter = categoryAdapter
         }
         observeData()
-        categoryGroupClickListener()
     }
 
     private fun observeData(){
@@ -54,29 +53,13 @@ class InsertRoutineDialogFragment : BaseDialog<DialogDailyRoutineBinding>(R.layo
                 }
                 launch {
                     viewModel.categoryList.collect{
-                        if (it.isEmpty()) return@collect
-                        if (binding.categoryGroup.isEmpty()) {
-                            it.forEach {
-                                binding.categoryGroup.addCheckableChip(it.name)
-                            }
-                        } else {
-                            binding.categoryGroup.addCheckableChip(it.last().name)
-                        }
+                        categoryAdapter.submitList(it)
                     }
                 }
             }
         }
     }
 
-    private fun categoryGroupClickListener() {
-        binding.categoryGroup.setOnCheckedStateChangeListener { group, checkedIdList ->
-            viewModel.categoryText = if(checkedIdList.isEmpty()){
-                ""
-            }else{
-                group.findViewById<Chip>(checkedIdList.first()).text.toString()
-            }
-        }
-    }
     companion object {
         const val TAG = "insert_routine_dialog"
     }
