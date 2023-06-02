768b24535ecb7403c456f5cb749918a4267fda98
hegunhee <gunhee0072@gmail.com>
2023-06-02 14:49:48 +0900

    [category-recyclerView] feat : CategoryActionHandler 구현

--- feature/daily/src/main/java/com/hegunhee/daily/insert/InsertRoutineDialogViewModel.kt
+++ feature/daily/src/main/java/com/hegunhee/daily/insert/InsertRoutineDialogViewModel.kt
@@ -8,6 +8,7 @@ import com.example.domain.usecase.category.GetAllCategoryListByFlowUseCase
 import com.example.domain.usecase.routine.GetRoutineListByDateUseCase
 import com.example.domain.usecase.routine.InsertRoutineUseCase
 import com.example.domain.usecase.routine.getTodayDate
+import com.hegunhee.category.CategoryActionHandler
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
@@ -18,7 +19,7 @@ class InsertRoutineDialogViewModel @Inject constructor(
     private val getRoutineListByDateUseCase: GetRoutineListByDateUseCase,
     private val insertRoutineUseCase: InsertRoutineUseCase,
     private val getAllCategoryListByFlowUseCase: GetAllCategoryListByFlowUseCase
-) : ViewModel(), InsertRoutineActionHandler {
+) : ViewModel(), InsertRoutineActionHandler,CategoryActionHandler {
 
     val routineText : MutableStateFlow<String> = MutableStateFlow<String>("")
 
@@ -28,11 +29,24 @@ class InsertRoutineDialogViewModel @Inject constructor(
     private val _toastMessage : MutableSharedFlow<String> = MutableSharedFlow<String>()
     val toastMessage : SharedFlow<String> = _toastMessage.asSharedFlow()
 
-    private val _categoryList : Flow<List<Category>> = getAllCategoryListByFlowUseCase()
+    private val _selectedCategory : MutableStateFlow<Category> = MutableStateFlow(Category(""))
+    private val selectedCategory : StateFlow<Category> = _selectedCategory.asStateFlow()
+
+    private val _categoryList : Flow<List<Category>> = getAllCategoryListByFlowUseCase().combine(selectedCategory) { categoryList, category ->
+        if(category.name.isBlank()){
+            categoryList
+        }else{
+            categoryList.map {
+                if(it.name == category.name){
+                    Category(it.name,category.isSelected)
+                }else{
+                    Category(it.name)
+                }
+            }
+        }
+    }
     val categoryList : Flow<List<Category>>
     get() = _categoryList
-
-    var categoryText : String = ""
 
     private val emptyMessage = "입력이 비어있습니다."
     private val sameRoutineMessage = "중복된 루틴입니다."
@@ -54,6 +68,11 @@ class InsertRoutineDialogViewModel @Inject constructor(
                 if(routineList.map(Routine::text).contains(routineText)){
                     _toastMessage.emit(sameRoutineMessage)
                 }else{
+                    val categoryText = if(selectedCategory.value.isSelected){
+                        selectedCategory.value.name
+                    }else{
+                        ""
+                    }
                     insertRoutineUseCase(Routine(date= getTodayDate(),text = routineText, category = categoryText))
                     _navigateActions.emit(InsertRoutineNavigationAction.DismissDialog)
                 }
@@ -64,6 +83,12 @@ class InsertRoutineDialogViewModel @Inject constructor(
     override fun openInsertCategoryDialog() {
         viewModelScope.launch {
             _navigateActions.emit(InsertRoutineNavigationAction.InsertCategoryDialog)
+        }
+    }
+
+    override fun onClickCategory(categoryText: String, isCategorySelected: Boolean) {
+        viewModelScope.launch {
+            _selectedCategory.value = Category(categoryText,!isCategorySelected)
         }
     }
 }
