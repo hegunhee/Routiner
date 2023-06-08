4e9d3d3794101a01f7f605f599a315f0a26d2913
hegunhee <gunhee0072@gmail.com>
2023-06-08 17:20:33 +0900

    [refactor-repeat] feat : 요일 정보를 담아서 Adapter를 만드는 BindingAdapter 작성

--- feature/repeat/src/main/java/com/hegunhee/repeat/RepeatAdapterBindingAdapters.kt
+++ feature/repeat/src/main/java/com/hegunhee/repeat/RepeatAdapterBindingAdapters.kt
@@ -1,17 +1,13 @@
 package com.hegunhee.repeat
 
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.chip.ChipGroup
 import com.hegunhee.common.util.addChip
 
 @BindingAdapter("setDayOfWeekCategory")
-fun ChipGroup.setDayOfWeekCategory(dayOfWeekList : List<String>){
-    removeAllViews()
-    addChipList(dayOfWeekList)
-}
-
-private fun ChipGroup.addChipList(list : List<String>){
-    list.forEach{
-        addChip(it)
+fun RecyclerView.setDayOfWeekCategory(dayOfWeekList : List<String>){
+    this.adapter = RepeatRoutineDateAdapter().apply {
+        submitList(dayOfWeekList)
     }
 }
