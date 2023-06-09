ed74473a7f9d73171a07bc0a11c25e3949bb8dc8
hegunhee <gunhee0072@gmail.com>
2023-06-09 14:27:09 +0900

    [refactor-repeat] refactor : CategoryActionHandler 구현 및 카테고리 관련 로직 수정

--- feature/repeat/src/main/java/com/hegunhee/repeat/insert/InsertRepeatRoutineDialogViewModel.kt
+++ feature/repeat/src/main/java/com/hegunhee/repeat/insert/InsertRepeatRoutineDialogViewModel.kt
@@ -8,6 +8,7 @@ import com.example.domain.model.Routine
 import com.example.domain.usecase.category.GetAllCategoryListByFlowUseCase
 import com.example.domain.usecase.repeatRoutine.InsertRepeatRoutineUseCase
 import com.example.domain.usecase.routine.InsertRoutineUseCase
+import com.hegunhee.category.CategoryActionHandler
 import com.hegunhee.common.util.getTodayDate
 import com.hegunhee.common.util.getTodayDayOfWeekFormatedKorean
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,23 +18,37 @@ import javax.inject.Inject
 
 @HiltViewModel
 class InsertRepeatRoutineDialogViewModel @Inject constructor(
-    private val allCategoryListByFlowUseCase: GetAllCategoryListByFlowUseCase,
+    private val getAllCategoryListByFlowUseCase: GetAllCategoryListByFlowUseCase,
     private val insertRoutineUseCase: InsertRoutineUseCase,
     private val insertRepeatRoutineUseCase: InsertRepeatRoutineUseCase
-) : ViewModel() , InsertRepeatRoutineActionHandler{
+) : ViewModel() , InsertRepeatRoutineActionHandler,CategoryActionHandler {
 
     val repeatRoutineText : MutableStateFlow<String> = MutableStateFlow<String>("")
 
-    var categoryText : String = ""
-
     var dayOfWeekList : List<String> = emptyList()
 
-    private val _categoryList : Flow<List<Category>> = allCategoryListByFlowUseCase()
-    val categoryList : Flow<List<Category>>
-    get() = _categoryList
 
     private val _navigationActions : MutableSharedFlow<InsertRepeatRoutineNavigationAction> = MutableSharedFlow<InsertRepeatRoutineNavigationAction>()
     val navigationActions : SharedFlow<InsertRepeatRoutineNavigationAction> = _navigationActions.asSharedFlow()
+
+    private val _selectedCategory : MutableStateFlow<Category> = MutableStateFlow(Category(""))
+    private val selectedCategory : StateFlow<Category> = _selectedCategory.asStateFlow()
+
+    private val _categoryList : Flow<List<Category>> = getAllCategoryListByFlowUseCase().combine(selectedCategory) { categoryList, category ->
+        if(category.name.isBlank()){
+            categoryList
+        }else{
+            categoryList.map {
+                if(it.name == category.name){
+                    Category(it.name,category.isSelected)
+                }else{
+                    Category(it.name)
+                }
+            }
+        }
+    }
+    val categoryList : Flow<List<Category>>
+        get() = _categoryList
 
     private val _toastMessage : MutableSharedFlow<String> = MutableSharedFlow<String>()
     val toastMessage : SharedFlow<String> = _toastMessage.asSharedFlow()
@@ -58,6 +73,12 @@ class InsertRepeatRoutineDialogViewModel @Inject constructor(
                 _toastMessage.emit(emptyDayOfWeekMessage)
                 return@launch
             }
+            val categoryText = if(selectedCategory.value.isSelected){
+                selectedCategory.value.name
+            }else{
+                ""
+            }
+
             val repeatRoutine = RepeatRoutine(repeatRoutineText,dayOfWeekList,categoryText)
             if(getTodayDayOfWeekFormatedKorean() in dayOfWeekList){
                 insertRoutineUseCase(Routine(date = getTodayDate(), text = repeatRoutineText, category = categoryText))
@@ -74,5 +95,9 @@ class InsertRepeatRoutineDialogViewModel @Inject constructor(
         }
     }
 
-
+    override fun onClickCategory(categoryText: String, isCategorySelected: Boolean) {
+        viewModelScope.launch {
+            _selectedCategory.value = Category(categoryText,!isCategorySelected)
+        }
+    }
 }
