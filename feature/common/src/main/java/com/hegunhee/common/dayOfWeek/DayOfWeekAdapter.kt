070fcdc1a1b027ded22d8e0a5d6d2e88c0807a48
hegunhee <gunhee0072@gmail.com>
2023-06-10 12:15:20 +0900

    [refactor-repeat] feat : 요일 Adapter 생성

---
+++ feature/common/src/main/java/com/hegunhee/common/dayOfWeek/DayOfWeekAdapter.kt
@@ -0,0 +1,43 @@
+package com.hegunhee.common.dayOfWeek
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.domain.model.DayOfWeek
+import com.hegunhee.common.databinding.ItemDayOfWeekBinding
+
+class DayOfWeekAdapter(private val actionHandler: DayOfWeekActionHandler) : ListAdapter<DayOfWeek, DayOfWeekAdapter.DayOfWeekViewHolder>(diff_util){
+
+    inner class DayOfWeekViewHolder(private val binding : ItemDayOfWeekBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bindView(dayOfWeek : DayOfWeek) {
+            binding.dayOfWeek = dayOfWeek
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayOfWeekViewHolder {
+        return DayOfWeekViewHolder(ItemDayOfWeekBinding.inflate(LayoutInflater.from(parent.context),parent,false).apply {
+            actionHandler = this@DayOfWeekAdapter.actionHandler
+        })
+    }
+
+    override fun onBindViewHolder(holder: DayOfWeekViewHolder, position: Int) {
+        holder.bindView(getItem(position))
+    }
+
+
+    companion object {
+        val diff_util = object : DiffUtil.ItemCallback<DayOfWeek>() {
+            override fun areItemsTheSame(oldItem: DayOfWeek, newItem: DayOfWeek): Boolean {
+                return oldItem.date == newItem.date
+            }
+
+            override fun areContentsTheSame(oldItem: DayOfWeek, newItem: DayOfWeek): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
