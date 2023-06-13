902c228b8b00aefb7551bac5f51abacf9c593073
hegunhee <gunhee0072@gmail.com>
2023-06-13 09:18:58 +0900

    [refactor-repeat] refactor : 메뉴 관련 설정 삭제

--- feature/record/src/main/java/com/hegunhee/record/RecordFragment.kt
+++ feature/record/src/main/java/com/hegunhee/record/RecordFragment.kt
@@ -1,14 +1,12 @@
 package com.hegunhee.record
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.hegunhee.common.base.BaseFragment
 import com.hegunhee.record.databinding.FragmentRecordBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,7 +23,6 @@ class RecordFragment : BaseFragment<FragmentRecordBinding>(R.layout.fragment_rec
             viewmodel = viewModel
             recyclerView.adapter = adapter
         }
-        setHasOptionsMenu(true)
         observeData()
     }
 
