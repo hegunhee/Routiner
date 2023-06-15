698178938bba32784824a94d3d9e09a1540a15bf
hegunhee <gunhee0072@gmail.com>
2023-06-16 08:39:55 +0900

    [remove-chip] refactor : chipUtil 코드 삭제 및 import 구문 삭제

--- feature/repeat/src/main/java/com/hegunhee/repeat/RepeatAdapterBindingAdapters.kt
+++ feature/repeat/src/main/java/com/hegunhee/repeat/RepeatAdapterBindingAdapters.kt
@@ -2,8 +2,6 @@ package com.hegunhee.repeat
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.chip.ChipGroup
-import com.hegunhee.common.util.addChip
 
 @BindingAdapter("setDayOfWeekCategory")
 fun RecyclerView.setDayOfWeekCategory(dayOfWeekList : List<String>){
