96bd0469f5a72f1c911e580b4367e793c2c7fd39
hegunhee <gunhee0072@gmail.com>
2023-06-16 09:14:56 +0900

    [refactor-domain] refactor : UseCase에서 객체를 만드는것이 아닌 int값만 넘기는것으로 수정

--- domain/src/main/java/com/example/domain/usecase/date/InsertDateUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/date/InsertDateUseCase.kt
@@ -1,6 +1,5 @@
 package com.example.domain.usecase.date
 
-import com.example.domain.model.Date
 import com.example.domain.repository.Repository
 import com.example.domain.usecase.UseCase
 import javax.inject.Inject
@@ -8,6 +7,6 @@ import javax.inject.Inject
 class InsertDateUseCase @Inject constructor(private val repository: Repository) : UseCase {
 
     suspend operator fun invoke(date : Int) {
-        repository.insertDate(Date(date))
+        repository.insertDate(date))
     }
 }
