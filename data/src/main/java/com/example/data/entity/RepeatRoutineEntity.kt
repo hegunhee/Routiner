437fcf96c2b1c982e35343b4dbce708eef1210a7
hegunhee <gunhee0072@gmail.com>
2023-06-16 09:15:17 +0900

    [refactor-domain] style : entity 들여쓰기 수정

--- data/src/main/java/com/example/data/entity/RepeatRoutineEntity.kt
+++ data/src/main/java/com/example/data/entity/RepeatRoutineEntity.kt
@@ -5,7 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class RepeatRoutineEntity(
-    @PrimaryKey var text : String = "",
+    @PrimaryKey
+    val text : String = "",
     val dayOfWeekList: List<String>,
     val category : String = ""
 )
