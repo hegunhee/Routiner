5d3601fa92d6147c4efafe5c1510323aaf48c275
hegunhee <gunhee0072@gmail.com>
2023-06-16 09:11:47 +0900

    [refactort-domain] refactor : 도메인 모듈에서는 레포지토리로 명령만 전달하는 코드로 변경

--- domain/src/main/java/com/example/domain/usecase/routine/InsertAllDailyRoutineFromRepeatRoutineUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/routine/InsertAllDailyRoutineFromRepeatRoutineUseCase.kt
@@ -1,19 +1,10 @@
 package com.example.domain.usecase.routine
 
-import com.example.domain.model.Routine
 import com.example.domain.repository.Repository
-import com.example.domain.usecase.repeatRoutine.GetAllRepeatRoutineListUseCase
 import javax.inject.Inject
 
-class InsertAllDailyRoutineFromRepeatRoutineUseCase @Inject constructor(
-    private val getAllRepeatRoutineListUseCase: GetAllRepeatRoutineListUseCase,
-    private val repository: Repository
-) {
+class InsertAllDailyRoutineFromRepeatRoutineUseCase @Inject constructor(private val repository: Repository) {
     suspend operator fun invoke(dayOfWeek: String) {
-        val list = getAllRepeatRoutineListUseCase().filter { it.dayOfWeekList.contains(dayOfWeek) }.map { Routine(getTodayDate(), it.text, category = it.category) }
-        if (list.isNotEmpty()) {
-            repository.insertAllRoutine(list)
-        }
+        repository.insertAllDailyRoutineFromRepeatRoutine(dayOfWeek)
     }
-}
 }
