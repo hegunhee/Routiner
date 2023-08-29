18d0f2087fb51cc7430a8c75d552e30b352aa060
hegunhee <gunhee0072@gmail.com>
2023-08-29 15:19:04 +0900

    [viewModel-refactor] refactor : 알람 수신 스위치 변경 이벤트를 뷰모델에서 처리

--- feature/setting/src/main/java/com/hegunhee/setting/SettingFragment.kt
+++ feature/setting/src/main/java/com/hegunhee/setting/SettingFragment.kt
@@ -2,13 +2,11 @@ package com.hegunhee.setting
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import com.hegunhee.common.base.BaseFragment
 import com.hegunhee.setting.databinding.FragmentSettingBinding
 import dagger.hilt.android.AndroidEntryPoint
-
 
 @AndroidEntryPoint
 class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
@@ -18,25 +16,11 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_s
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             viewmodel = viewModel
-            notiSwitchButton.isChecked = viewModel.getNotiSendValue()
         }
         setActionBarTitle()
-        initListener()
     }
 
     private fun setActionBarTitle(){
         (requireActivity() as AppCompatActivity).supportActionBar?.title = "설정"
-    }
-
-
-    private fun initListener() = with(binding){
-        notiSwitchButton.setOnCheckedChangeListener { _, switch ->
-            viewModel.setNotiSendValue(switch)
-            if(switch){
-                Toast.makeText(requireContext(), "알람 수신이 설정되었습니다.", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(requireContext(), "알람 수신이 해제되었습니다.", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 }
--- feature/setting/src/main/java/com/hegunhee/setting/SettingViewModel.kt
+++ feature/setting/src/main/java/com/hegunhee/setting/SettingViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.domain.usecase.notification.GetNotiSendValueUseCase
 import com.example.domain.usecase.notification.SetNotiSendValueUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,11 +13,12 @@ class SettingViewModel @Inject constructor(
     private val setNotiSendValueUseCase: SetNotiSendValueUseCase
 ) : ViewModel(){
 
+    val notiSendValue : MutableStateFlow<Boolean> = MutableStateFlow(getNotiSendValueUseCase())
 
-    fun getNotiSendValue() : Boolean{
-        return getNotiSendValueUseCase()
+    fun onNotiCheckedChanged(checked : Boolean) {
+        notiSendValue.value = checked
+        setNotiSendValue(checked)
     }
-
     fun setNotiSendValue(isActive : Boolean){
         setNotiSendValueUseCase(isActive)
     }
