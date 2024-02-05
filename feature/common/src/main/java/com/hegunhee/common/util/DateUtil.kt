8bf29c4f976a2eb101ede0b3af74f053d596ba62
hegunhee <gunhee0072@gmail.com>
2024-02-05 21:02:12 +0900

    [feature-refactor] build : minSdk를 26으로 targetSdk를 33으로 설정

--- feature/common/src/main/java/com/hegunhee/common/util/DateUtil.kt
+++ feature/common/src/main/java/com/hegunhee/common/util/DateUtil.kt
@@ -1,39 +1,17 @@
 package com.hegunhee.common.util
 
-import android.os.Build
-import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
 import java.util.*
 
 fun getTodayDate(): Int {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")).toInt()
-    }else {
-        val currentDate = getCurrentKoreanCalender().time
-        val dateFormat = SimpleDateFormat("yyyyMMdd", Locale.KOREA)
-        return dateFormat.format(currentDate).toInt()
-    }
+    return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")).toInt()
 }
 
 
 
 
 fun getTodayDayOfWeekFormatedKorean(): String  {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        LocalDateTime.now().dayOfWeek.getDisplayName(TextStyle.NARROW, Locale.KOREAN)
-    }else {
-        val calendar = getCurrentKoreanCalender()
-        val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
-        val dayOfWeekList = listOf<String>("", "일", "월", "화", "수", "목", "금", "토")
-        return dayOfWeekList[dayOfWeek]
-    }
-}
-
-private fun getCurrentKoreanCalender() : Calendar  {
-    val calendar = Calendar.getInstance()
-    calendar.time = Date()
-    calendar.add(Calendar.HOUR_OF_DAY,9)
-    return calendar
+    return LocalDateTime.now().dayOfWeek.getDisplayName(TextStyle.NARROW, Locale.KOREAN)
 }
