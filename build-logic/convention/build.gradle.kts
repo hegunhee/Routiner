444a7766c6b120f1755d31979e5a897eed1f7207
hegunhee <gunhee0072@gmail.com>
2024-02-07 00:55:16 +0900

    [build-logic-refactor] refactor : feature용 gradle설정을 gradle파일로 생성

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -2,15 +2,15 @@ plugins {
     `kotlin-dsl`
     `kotlin-dsl-precompiled-script-plugins`
 }
-
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies{
-    compileOnly(libs.android.gradlePlugin)
-    compileOnly(libs.kotlin.gradlePlugin)
+    implementation(libs.android.gradlePlugin)
+    implementation(libs.kotlin.gradlePlugin)
+    implementation(libs.hilt.plugin)
 }
 gradlePlugin{
     plugins{
@@ -19,16 +19,12 @@ gradlePlugin{
             implementationClass = "com.example.convention.AndroidLibraryConventionPlugin"
         }
         register("androidHilt"){
-            id = "android.routiner.hilt"
+            id = "routiner.android.hilt"
             implementationClass = "com.example.convention.project.HiltKotlinPlugin"
         }
         register("androidApplication"){
             id = "android.application"
             implementationClass = "com.example.convention.AndroidApplicationConventionPlugin"
-        }
-        register("androidFeature"){
-            id = "android.feature"
-            implementationClass = "com.example.convention.AndroidFeatureConventionPlugin"
         }
     }
 }
