7f1de97e03964c2b4bec818718079e55925d571d
hegunhee <gunhee0072@gmail.com>
2024-02-06 14:40:45 +0900

    [build-logic] build : project의 build.gradle plugins를 id가 아닌 alias로 지정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+@file:Suppress("DSL_SCOPE_VIOLATION")
 buildscript {
     dependencies {
         classpath(libs.hilt.plugin)
@@ -6,10 +6,10 @@ buildscript {
 }
 
 plugins {
-    id ("com.android.application") version "8.0.2" apply false
-    id ("com.android.library") version "8.0.2" apply false
-    id ("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id ("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
 }
 
 task("clean",Delete::class) {
