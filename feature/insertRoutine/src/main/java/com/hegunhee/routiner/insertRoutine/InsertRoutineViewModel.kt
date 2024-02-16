cdff95070f750aaebbfb0c4faff38f4e19e4095d
hegunhee <gunhee0072@gmail.com>
2024-02-16 13:58:15 +0900

    [routine-insert-ui] feat : InsertRoutine Fragment추가 및 navGraph에 연동

---
+++ feature/insertRoutine/src/main/java/com/hegunhee/routiner/insertRoutine/InsertRoutineViewModel.kt
@@ -0,0 +1,9 @@
+package com.hegunhee.routiner.insertRoutine
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class InsertRoutineViewModel @Inject constructor() : ViewModel() {
+}
