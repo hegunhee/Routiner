118558bb0f6c3fed6cade1972814fb2d73f833d0
hegunhee <gunhee0072@gmail.com>
2024-02-17 23:47:29 +0900

    [routine-insert-ui] feat : 루틴추가화면 상단 타이틀과 버튼 추가

--- feature/insertRoutine/src/main/java/com/hegunhee/routiner/insertRoutine/InsertRoutineFragment.kt
+++ feature/insertRoutine/src/main/java/com/hegunhee/routiner/insertRoutine/InsertRoutineFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.hegunhee.routiner.insertRoutine.databinding.FragmentInsertRoutineBinding
@@ -29,5 +30,10 @@ class InsertRoutineFragment : Fragment(){
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setActionBarTitle()
+    }
+
+    private fun setActionBarTitle(){
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = "루틴 추가"
     }
 }
