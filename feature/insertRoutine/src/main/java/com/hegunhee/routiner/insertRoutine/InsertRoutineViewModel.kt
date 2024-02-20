5e75f4072c9bde141ea9d693cafc342c9add2be5
hegunhee <gunhee0072@gmail.com>
2024-02-20 09:47:17 +0900

    [routine-insert-ui] feat : 루틴추가화면에서 카테고리 추가, 설정 코드 추가

--- feature/insertRoutine/src/main/java/com/hegunhee/routiner/insertRoutine/InsertRoutineViewModel.kt
+++ feature/insertRoutine/src/main/java/com/hegunhee/routiner/insertRoutine/InsertRoutineViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.model.Category
 import com.example.domain.usecase.category.GetAllCategoryListByFlowUseCase
+import com.example.domain.usecase.category.InsertCategoryUseCase
 import com.hegunhee.category.CategoryActionHandler
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -12,18 +13,39 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class InsertRoutineViewModel @Inject constructor(
-    private val getAllCategoryListByFlowUseCase: GetAllCategoryListByFlowUseCase
+    private val getAllCategoryListByFlowUseCase: GetAllCategoryListByFlowUseCase,
+    private val insertCategoryUseCase: InsertCategoryUseCase
 ) : ViewModel(), InsertRoutineActionHandler, CategoryActionHandler {
 
     val routineQuery : MutableStateFlow<String> = MutableStateFlow("")
 
-    val categoryList : StateFlow<List<Category>> = getAllCategoryListByFlowUseCase().stateIn(
+    val categoryQuery : MutableStateFlow<String> = MutableStateFlow("")
+
+    private val _selectedCategory : MutableStateFlow<Category> = MutableStateFlow(Category(""))
+    private val selectedCategory : StateFlow<Category> = _selectedCategory.asStateFlow()
+
+
+    val categoryList : StateFlow<List<Category>> = getAllCategoryListByFlowUseCase().combine(selectedCategory) { categoryList, selectedCategory ->
+        if(selectedCategory.name.isBlank()) {
+            categoryList
+        }else {
+            categoryList.map {
+                if(it.name == selectedCategory.name){
+                    Category(it.name,selectedCategory.isSelected)
+                }else{
+                    Category(it.name)
+                }
+            }
+        }
+    }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5000L),
         initialValue = emptyList()
@@ -38,7 +60,20 @@ class InsertRoutineViewModel @Inject constructor(
         }
     }
 
-    override fun onClickCategory(categoryText: String, isCategorySelected: Boolean) {
+    override fun onCategoryAddClick() {
+        val query = categoryQuery.value
+        if(query.isBlank()) {
+            return
+        }
+        viewModelScope.launch {
+            insertCategoryUseCase(Category(name = query))
+        }
 
+    }
+
+    override fun onClickCategory(categoryText: String, isCategorySelected: Boolean) {
+        viewModelScope.launch {
+            _selectedCategory.value = Category(categoryText,!isCategorySelected)
+        }
     }
 }
