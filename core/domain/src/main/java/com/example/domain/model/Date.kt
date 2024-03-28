c9cc0d37a2fe257d1c2dc36828d4783afa63f1a6
hegunhee <gunhee0072@gmail.com>
2024-03-28 09:55:04 +0900

    [record-calendar] feat : Date 도메인 모델 날짜 데이터 추가

--- core/domain/src/main/java/com/example/domain/model/Date.kt
+++ core/domain/src/main/java/com/example/domain/model/Date.kt
@@ -1,3 +1,22 @@
 package com.example.domain.model
 
-data class Date(val date : Int)
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.Locale
+
+data class Date(val date : Int) {
+
+    val year : String = getLocalDate(date.toString()).year.toString()
+    val month : String = getLocalDate(date.toString()).monthValue.toString()
+    val dayOfMonth : String = getLocalDate(date.toString()).dayOfMonth.toString()
+    val dayOfWeek : String = getLocalDate(date.toString()).dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+    val desc : String = "${year}년 ${month}월 $dayOfMonth"
+    companion object {
+        private val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+        
+        private fun getLocalDate(date : String) : LocalDate {
+            return LocalDate.parse(date, formatter)
+        }
+    }
+}
