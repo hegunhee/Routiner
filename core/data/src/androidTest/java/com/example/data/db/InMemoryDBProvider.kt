79265a282c58d278fb3b22d80b616c2e64ff21bf
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-02-27 12:04:59 +0900

    Room Test (#68)
    * #67 - build : 예외처리와 보다 간결하게 테스트코드를 짜기위해 assertJ 추가
    
    예외에 대한 메시지 분석도 가능하고 보다 테스트코드를 읽기 쉽게 만들어줍니다
    
    * #67 - feat : DB Builder 생성
    
    dao마다 테스트를 해줘야하는데 이럴경우 DB를 만드는 코드가 중복이므로 Provider를 생성
    
    * #67 - test : CategoryDao 테스트코드 작성
    
    * #67 - test : DateDao 테스트 작성
    
    * #67 - test : RepeatRoutineDao 테스트코드 작성
    
    * #67 - style : when문을 동사의 현재진행형으로 변경
    
    * #67 - test : ReviewDao 테스트 작성
    
    * #67 - test : RoutineDao 테스트코드 작성

---
+++ core/data/src/androidTest/java/com/example/data/db/InMemoryDBProvider.kt
@@ -0,0 +1,14 @@
+package com.example.data.db
+
+import android.content.Context
+import androidx.room.Room
+
+object InMemoryDBProvider {
+
+    fun provideInMemoryDB(context: Context): RoutinerDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            context, RoutinerDatabase::class.java
+        ).build()
+    }
+
+}
---
+++ core/data/src/androidTest/java/com/example/data/db/dao/CategoryDaoTest.kt
@@ -0,0 +1,103 @@
+package com.example.data.db.dao
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.data.db.InMemoryDBProvider.provideInMemoryDB
+import com.example.data.db.RoutinerDatabase
+import com.example.data.entity.CategoryEntity
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CategoryDaoTest {
+
+    private lateinit var sut: CategoryDao
+    private lateinit var db: RoutinerDatabase
+
+    @Before
+    fun initDB() {
+        db = provideInMemoryDB(ApplicationProvider.getApplicationContext())
+        sut = db.categoryDao()
+    }
+
+    @Throws
+    @After
+    fun tearDown() {
+        db.close()
+    }
+
+    @Test
+    fun givenCategoryEntity_whenInserting_thenWorksFine() {
+        runBlocking {
+            // given
+            val entity = CategoryEntity("운동")
+
+            // when
+            sut.insertCategory(entity)
+            val insertedEntity = sut.getAllCategoryListByFlow().first()[0]
+
+            // then
+            assertEquals(insertedEntity, entity)
+        }
+    }
+
+    @Test
+    fun givenCategoryEntity_whenInsertTwice_thenIgnoreSecondInsert() {
+        runBlocking {
+            // given
+            val entity = CategoryEntity("운동")
+            val previousCount = sut.getAllCategoryListByFlow().first().size
+
+            // when
+            sut.insertCategory(entity)
+            sut.insertCategory(entity)
+            val entities =
+                sut.getAllCategoryListByFlow().first().filter { it.name == entity.name }
+
+            // then
+            assertEquals(entities.size, previousCount + 1)
+        }
+    }
+
+    @Test
+    fun givenCategoryEntity_whenRemoving_thenWorksFine() {
+        runBlocking {
+            // given
+            val entity = CategoryEntity("운동")
+            sut.insertCategory(entity)
+            val previousCount = sut.getAllCategoryListByFlow().first().size
+
+            // when
+            sut.removeCategory(entity)
+            val count = sut.getAllCategoryListByFlow().first().size
+
+            // then
+            assertEquals(count, previousCount - 1)
+        }
+    }
+
+    @Test
+    fun givenTwoEntities_whenDeleting_thenWorksFine() {
+        runBlocking {
+            // given
+            val previousEntities = listOf(CategoryEntity("운동"), CategoryEntity("게임"))
+            previousEntities.forEach {
+                sut.insertCategory(it)
+            }
+            val entitiesFlow = sut.getAllCategoryListByFlow()
+
+            // when
+            sut.removeCategory(previousEntities[0])
+            val entities = entitiesFlow.first()
+
+            // then
+            assertEquals(entities.size, previousEntities.size - 1)
+            assertEquals(entities[0],previousEntities.first { it.name == entities[0].name })
+        }
+    }
+}
