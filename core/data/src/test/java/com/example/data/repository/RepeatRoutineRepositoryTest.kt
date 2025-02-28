8d69c46a37aabe097db5c2dbbd46a0f4b1c4e395
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-02-28 15:33:16 +0900

    Repository 슬라이스 테스트 (#70)
    * #69 - feat : core:data 모듈에 test 패키지 생성
    
    기존에 왜인지는 모르겠지만 존재하지 않았음
    
    * #69 - style : 레포지토리 분리를 편하게 하기 위해서 레포지토리 코드 배치 변경
    
    동일한 엔티티나 모델을 사용할경우 같이 묶고 CRUD 순으로 정렬
    그리고 엔티티나 모델별로 두줄 띄움
    
    * #69 - test : RoutineRepository 테스트코드 작성
    
    현재 RoutineRepository가 존재하지는 않지만
    추후에 모델별로 분리할 예정이라 레포지토리 테스트클래스를 분리
    
    현재 반환형이 Unit이고 then문에 assertThat문이 없는경우는
    추후 값이 추가될 예정
    
    * #69 - test : DateRepository 테스트코드 작성
    
    * #69 - test : 루틴 테스트에서 Get테스트의 경우 given단계에서 모델클래스가 아닌 엔티티클래스를 생성
    
    엔티티 클래스를 만들어서 데이터를 주입해줘야
    보다 테스트케이스가 매끄러워지는것같아서 변경
    
    * #69 - test : ReviewRepository 테스트코드 작성
    
    * #69 - test : RepeatRoutineRepository 테스트코드 작성
    
    * #69 - test : CategoryRepository 테스트코드 작성
    
    * #69 - test : SettingRepository 테스트코드 작성
    
    localDataSource에서 리턴값이 없는 함수는 doNothing을 사용함
    
    * #69 - style : EOF 수정

---
+++ core/data/src/test/java/com/example/data/repository/RepeatRoutineRepositoryTest.kt
@@ -0,0 +1,107 @@
+package com.example.data.repository
+
+import com.example.data.dataSource.local.DefaultLocalDataSource
+import com.example.data.entity.RepeatRoutineEntity
+import com.example.data.mapper.toRepeatRoutineEntity
+import com.example.domain.model.RepeatRoutine
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class RepeatRoutineRepositoryTest {
+
+    @InjectMocks
+    private lateinit var sut: DefaultRepository
+
+    @Mock
+    private lateinit var localDateSource: DefaultLocalDataSource
+
+    @Test
+    fun givenDayOfWeek_whenInsertAllRoutine_thenWorksFine() {
+        runBlocking {
+            // given
+            val dayOfWeek = "월"
+            whenever(localDateSource.getAllRepeatRoutineList()).thenReturn(listOf())
+            whenever(localDateSource.insertAllRoutine(listOf())).thenReturn(Unit)
+
+            // when
+            sut.insertAllDailyRoutineFromRepeatRoutine(dayOfWeek)
+
+            // then
+            verify(localDateSource).getAllRepeatRoutineList()
+            verify(localDateSource).insertAllRoutine(listOf())
+        }
+    }
+
+    @Test
+    fun givenRepeatRoutine_whenInsert_thenWorksFine() {
+        runBlocking {
+            // given
+            val repeatRoutine = RepeatRoutine(dayOfWeekList = listOf("월"))
+            whenever(localDateSource.insertRepeatRoutine(repeatRoutine.toRepeatRoutineEntity())).thenReturn(Unit)
+
+            // when
+            sut.insertRepeatRoutine(RepeatRoutine(dayOfWeekList = listOf("월")))
+
+            // then
+            verify(localDateSource).insertRepeatRoutine(repeatRoutine.toRepeatRoutineEntity())
+        }
+    }
+
+    @Test
+    fun given_whenGetAllRepeatRoutinesByFlow_thenWorksFine() {
+        runBlocking {
+            // given
+            val repeatRoutineEntitiesByFlow = flowOf(listOf(RepeatRoutineEntity(dayOfWeekList = listOf("월"))))
+            whenever(localDateSource.getAllRepeatRoutineListByFlow()).thenReturn(repeatRoutineEntitiesByFlow)
+
+            // when
+            val repeatRoutinesByFlow = sut.getAllRepeatRoutineListByFlow()
+
+            // then
+            assertThat(repeatRoutinesByFlow.first().size).isEqualTo(repeatRoutineEntitiesByFlow.first().size)
+            verify(localDateSource).getAllRepeatRoutineListByFlow()
+        }
+    }
+
+    @Test
+    fun given_whenGetAllRepeatRoutines_thenWorksFine() {
+        runBlocking {
+            // given
+            val repeatRoutineEntities = listOf(RepeatRoutineEntity(dayOfWeekList = listOf("월")))
+            whenever(localDateSource.getAllRepeatRoutineList()).thenReturn(repeatRoutineEntities)
+
+            // when
+            val repeatRoutines = sut.getAllRepeatRoutineList()
+
+            // then
+            assertThat(repeatRoutines.size).isEqualTo(repeatRoutineEntities.size)
+            verify(localDateSource).getAllRepeatRoutineList()
+        }
+    }
+
+    @Test
+    fun givenText_whenDelete_thenWorksFine() {
+        runBlocking {
+            // given
+            val text = ""
+            whenever(localDateSource.deleteRepeatRoutine(text)).thenReturn(Unit)
+
+            // when
+            sut.deleteRepeatRoutine(text)
+
+            // then
+            verify(localDateSource).deleteRepeatRoutine(text)
+        }
+    }
+
+}
---
+++ core/data/src/test/java/com/example/data/repository/ReviewRepositoryTest.kt
@@ -0,0 +1,89 @@
+package com.example.data.repository
+
+import com.example.data.dataSource.local.DefaultLocalDataSource
+import com.example.data.entity.ReviewEntity
+import com.example.data.mapper.toReviewEntity
+import com.example.domain.model.Review
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class ReviewRepositoryTest {
+
+    @InjectMocks
+    private lateinit var sut: DefaultRepository
+
+    @Mock
+    private lateinit var localDateSource: DefaultLocalDataSource
+
+    private val date = 20250228
+
+    @Test
+    fun givenReviewDate_whenGetReview_thenWorksFine() {
+        runBlocking {
+            // given
+            val reviewEntity = ReviewEntity(date, "")
+            whenever(localDateSource.getReviewOrNullByDate(date)).thenReturn(reviewEntity)
+
+            // when
+            val review = sut.getReviewOrNullByDate(date)
+
+            // then
+            assertThat(review?.date).isEqualTo(date)
+            verify(localDateSource).getReviewOrNullByDate(date)
+        }
+    }
+
+    @Test
+    fun givenNonExistentDate_whenGetReview_thenReturnsNull() {
+        runBlocking {
+            // given
+            val nonExistentDate = -1
+            whenever(localDateSource.getReviewOrNullByDate(nonExistentDate)).thenReturn(null)
+
+            // when
+            val review = sut.getReviewOrNullByDate(nonExistentDate)
+
+            // then
+            assertThat(review).isNull()
+            verify(localDateSource).getReviewOrNullByDate(nonExistentDate)
+        }
+    }
+
+    @Test
+    fun givenReview_whenInsert_thenWorksFine() {
+        runBlocking {
+            // given
+            val review = Review(date, "")
+            whenever(localDateSource.insertReview(review.toReviewEntity())).thenReturn(Unit)
+
+            // when
+            sut.insertReview(review)
+
+            // then
+            verify(localDateSource).insertReview(review.toReviewEntity())
+        }
+    }
+
+    @Test
+    fun givenReview_whenDelete_thenWorksFine() {
+        runBlocking {
+            // given
+            val review = Review(date, "")
+            whenever(localDateSource.deleteReview(review.toReviewEntity())).thenReturn(Unit)
+
+            // when
+            sut.deleteReview(review)
+
+            // then
+            verify(localDateSource).deleteReview(review.toReviewEntity())
+        }
+    }
+}
