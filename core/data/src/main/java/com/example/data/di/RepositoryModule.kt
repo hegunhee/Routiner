be8d035cf7dd738b7a9e38342f196c1fb8bf4416
이건희 <leech9876@naver.com>
2025-03-01 14:39:10 +0900

    #76 - feat : RepeatRoutineRepository 분리 및 Module 등록

--- core/data/src/main/java/com/example/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/example/data/di/RepositoryModule.kt
@@ -1,9 +1,11 @@
 package com.example.data.di
 
+import com.example.data.repository.DefaultRepeatRoutineRepository
 import com.example.domain.repository.Repository
 import com.example.data.repository.DefaultRepository
 import com.example.data.repository.DefaultRoutineRepository
 import com.example.data.repository.DefaultSettingRepository
+import com.example.domain.repository.RepeatRoutineRepository
 import com.example.domain.repository.RoutineRepository
 import com.example.domain.repository.SettingRepository
 import dagger.Binds
@@ -18,20 +20,26 @@ abstract class RepositoryModule {
 
     @Singleton
     @Binds
+    abstract fun provideRepeatRoutineRepository(
+        defaultRepeatRoutineRepository: DefaultRepeatRoutineRepository
+    ): RepeatRoutineRepository
+
+    @Singleton
+    @Binds
     abstract fun provideDefaultRoutineRepository(
         defaultRoutineRepository: DefaultRoutineRepository
-    ) : RoutineRepository
+    ): RoutineRepository
 
     @Singleton
     @Binds
     abstract fun provideDefaultSettingRepository(
         defaultSettingRepository: DefaultSettingRepository
-    ) : SettingRepository
+    ): SettingRepository
 
     @Singleton
     @Binds
     abstract fun provideDefaultRepository(
         defaultRepository: DefaultRepository
-    ) : Repository
+    ): Repository
 
 }
