50949ab30db9c2ab5781d05de96ef9fca1564d9a
이건희 <leech9876@naver.com>
2025-03-01 15:02:50 +0900

    #76 - feat : 유즈케이스에서 CategoryRepository를 사용하도록 설정

--- core/domain/src/main/java/com/example/domain/usecase/category/GetAllCategoryListByFlowUseCase.kt
+++ core/domain/src/main/java/com/example/domain/usecase/category/GetAllCategoryListByFlowUseCase.kt
@@ -1,11 +1,11 @@
 package com.example.domain.usecase.category
 
 import com.example.domain.model.Category
-import com.example.domain.repository.Repository
+import com.example.domain.repository.CategoryRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetAllCategoryListByFlowUseCase @Inject constructor(private val repository: Repository) {
+class GetAllCategoryListByFlowUseCase @Inject constructor(private val repository: CategoryRepository) {
 
     operator fun invoke() : Flow<List<Category>> {
         return repository.getAllCategoryListByFlow()
--- core/domain/src/main/java/com/example/domain/usecase/category/InsertCategoryUseCase.kt
+++ core/domain/src/main/java/com/example/domain/usecase/category/InsertCategoryUseCase.kt
@@ -1,10 +1,10 @@
 package com.example.domain.usecase.category
 
 import com.example.domain.model.Category
-import com.example.domain.repository.Repository
+import com.example.domain.repository.CategoryRepository
 import javax.inject.Inject
 
-class InsertCategoryUseCase @Inject constructor(private val repository: Repository) {
+class InsertCategoryUseCase @Inject constructor(private val repository: CategoryRepository) {
 
     suspend operator fun invoke(category: Category){
         repository.insertCategory(category)
