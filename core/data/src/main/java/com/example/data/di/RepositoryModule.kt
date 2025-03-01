69d41b0ffaed8007c524a880290546c52b19e850
이건희 <leech9876@naver.com>
2025-03-01 14:51:03 +0900

    #76 - feat : DateRepository 분리 및 Module 등록

--- core/data/src/main/java/com/example/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/example/data/di/RepositoryModule.kt
@@ -1,11 +1,13 @@
 package com.example.data.di
 
+import com.example.data.repository.DefaultDateRepository
 import com.example.data.repository.DefaultRepeatRoutineRepository
 import com.example.domain.repository.Repository
 import com.example.data.repository.DefaultRepository
 import com.example.data.repository.DefaultReviewRepository
 import com.example.data.repository.DefaultRoutineRepository
 import com.example.data.repository.DefaultSettingRepository
+import com.example.domain.repository.DateRepository
 import com.example.domain.repository.RepeatRoutineRepository
 import com.example.domain.repository.ReviewRepository
 import com.example.domain.repository.RoutineRepository
@@ -19,6 +21,12 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 abstract class RepositoryModule {
+
+    @Singleton
+    @Binds
+    abstract fun provideDateRepository(
+        defaultDateRepository: DefaultDateRepository
+    ): DateRepository
 
     @Singleton
     @Binds
---
+++ core/domain/src/main/java/com/example/domain/repository/DateRepository.kt
@@ -0,0 +1,11 @@
+package com.example.domain.repository
+
+import com.example.domain.model.Date
+
+interface DateRepository {
+
+    suspend fun insertDate(date : Int)
+
+    suspend fun getAllDateList() : List<Date>
+
+}
