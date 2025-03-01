4a992f0792fdbf99d95ecc1d7d378c635d1d8826
이건희 <leech9876@naver.com>
2025-03-02 00:31:29 +0900

    #82 - style : RepeatRoutine 관련 UseCase 클래스명 변경

--- core/domain/src/main/java/com/example/domain/usecase/repeatRoutine/GetRepeatRoutinesFlowUseCase.kt
+++ core/domain/src/main/java/com/example/domain/usecase/repeatRoutine/GetRepeatRoutinesFlowUseCase.kt
@@ -5,9 +5,10 @@ import com.example.domain.repository.RepeatRoutineRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetAllRepeatRoutineListByFlowUseCase @Inject constructor(private val repository: RepeatRoutineRepository) {
+class GetRepeatRoutinesFlowUseCase @Inject constructor(private val repository: RepeatRoutineRepository) {
 
     operator fun invoke(): Flow<List<RepeatRoutine>> {
         return repository.getRepeatRoutinesFlow()
     }
+
 }
