c628f559fd6ed870676d12907cb88fae0ed84d19
이건희 <leech9876@naver.com>
2025-03-05 22:51:01 +0900

    #91 - feat : 네비게이션이 잘 작동하나 TestScreen 작성
    추후 Daily 화면이 작성되면 삭제 예정

---
+++ feature/main/src/main/java/com/example/main/TestScreen.kt
@@ -0,0 +1,11 @@
+package com.example.main
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+val TestRoute = "TEST"
+
+@Composable
+fun TestScreen() {
+    Text("TestScreen")
+}
