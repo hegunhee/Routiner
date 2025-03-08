08321fdd211b3b3332ab5d33f166c6b871d6f821
이건희 <leech9876@naver.com>
2025-03-08 14:05:39 +0900

    #93 - feat : MainScreen 화면 작성

--- core/designsystem/src/main/java/hegunhee/routiner/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/hegunhee/routiner/designsystem/theme/Color.kt
@@ -3,3 +3,5 @@ package hegunhee.routiner.designsystem.theme
 import androidx.compose.ui.graphics.Color
 
 val Teal200 = Color(0xFF03DAC5)
+
+val DarkGreen = Color(0xFF4CAF50)
--- feature/main/src/main/java/com/example/main/screen/MainRoot.kt
+++ feature/main/src/main/java/com/example/main/screen/MainRoot.kt
@@ -1,9 +1,18 @@
 package com.example.main.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.main.R
+import hegunhee.routiner.designsystem.theme.DarkGreen
 
 @Composable
 fun MainRootScreen(
@@ -17,10 +26,22 @@ fun MainRootScreen(
 @Composable
 fun MainScreen(
     onNavigateDailyScreen: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Column {
-        Text("MainScreen입니다.")
+        Text(
+            stringResource(R.string.app_name),
+            modifier = modifier.padding(top = 100.dp, start = 30.dp),
+            fontSize = 40.sp,
+            color = DarkGreen
+        )
+        Text(
+            stringResource(R.string.app_description),
+            modifier = modifier.padding(top = 10.dp, start = 30.dp),
+            fontSize = 20.sp,
+        )
     }
+
 }
 
 
