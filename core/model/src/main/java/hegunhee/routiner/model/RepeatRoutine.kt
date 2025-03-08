7c242c73594a24f09721ab5ab454464b91d03739
이건희 <leech9876@naver.com>
2025-03-08 14:57:55 +0900

    #93 - feat: 반복 루틴이 특정 날짜를 포함하고있는지 확인하는 함수 추가

--- core/model/src/main/java/hegunhee/routiner/model/RepeatRoutine.kt
+++ core/model/src/main/java/hegunhee/routiner/model/RepeatRoutine.kt
@@ -4,4 +4,8 @@ data class RepeatRoutine(
     var text : String = "",
     val dayOfWeekList: List<String>,
     val category : String = ""
-)
+) {
+    fun containsDayOfWeek(dayOfWeek : String) : Boolean{
+        return dayOfWeekList.contains(dayOfWeek)
+    }
+}
