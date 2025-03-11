96f145a768df3ce35588fb0fe6281a5c71a87285
이건희 <leech9876@naver.com>
2025-03-11 11:16:54 +0900

    #98 - feat : 오늘의 할일이 없을때 화면 작성

--- feature/daily/src/main/java/com/hegunhee/daily/screen/DailyRootScreen.kt
+++ feature/daily/src/main/java/com/hegunhee/daily/screen/DailyRootScreen.kt
@@ -1,11 +1,15 @@
 package com.hegunhee.daily.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.sharp.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -15,9 +19,15 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.hegunhee.daily.R
@@ -53,6 +63,18 @@ fun DailyScreen(
             }
         )
 
+        when (uiState) {
+            DailyUiState.Init -> {}
+            DailyUiState.Empty -> {
+                DailyEmptyScreen(
+                    onClickAddRoutine = { }
+                )
+            }
+
+            is DailyUiState.Items -> {
+            }
+        }
+
         Spacer(modifier = modifier.weight(1f))
 
         FloatingActionButton(
@@ -66,6 +88,34 @@ fun DailyScreen(
 
     }
 
+}
+
+@Composable
+private fun ColumnScope.DailyEmptyScreen(
+    onClickAddRoutine: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Spacer(modifier = modifier.weight(1f))
+
+    Text(
+        text = stringResource(R.string.empty_string),
+        modifier = modifier
+            .align(Alignment.CenterHorizontally),
+        fontSize = 25.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+
+    Button(
+        onClick = onClickAddRoutine,
+        modifier = modifier.align(Alignment.CenterHorizontally),
+        shape = RectangleShape,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Gray
+        )
+    ) {
+        Text(stringResource(R.string.add_routine_string))
+    }
 }
 
 @Preview
