e2fcff6e29e3c8ad7e10a3ad167714c9540412d7
이건희 <leech9876@naver.com>
2025-03-17 09:12:44 +0900

    #100 - feat : InsertRoutine RootScreen 뼈대 작성

---
+++ feature/insertRoutine/src/main/java/com/hegunhee/routiner/insertRoutine/screen/InsertRoutineRootScreen.kt
@@ -0,0 +1,21 @@
+package com.hegunhee.routiner.insertRoutine.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun InsertRoutineRootScreen() {
+
+}
+
+@Composable
+fun InsertRoutineScreen() {
+    Text("InsertScreen")
+}
+
+@Preview
+@Composable
+fun InsertRoutineScreenPreview() {
+    InsertRoutineScreen()
+}
