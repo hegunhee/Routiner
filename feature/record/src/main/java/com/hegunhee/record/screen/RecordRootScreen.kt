235d12144fbd3eb41dc298d0e05a5f4f644a5b28
이건희 <leech9876@naver.com>
2025-03-20 10:37:05 +0900

    #105 - feat : RecordScreen에 TopAppBar 작성

--- feature/record/src/main/java/com/hegunhee/record/screen/RecordRootScreen.kt
+++ feature/record/src/main/java/com/hegunhee/record/screen/RecordRootScreen.kt
@@ -1,23 +1,48 @@
 package com.hegunhee.record.screen
 
+import android.icu.text.CaseMap.Title
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.hegunhee.record.R
 
 @Composable
 fun RecordRootScreen(
     onClickDrawer : () -> Unit
 ) {
     RecordScreen(
-        onClickDrawer
+        onClickDrawer = onClickDrawer
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecordScreen(
-    onClickDrawer: () -> Unit
+    date: String = "2025년 2월 26일",
+    onClickDrawer: () -> Unit,
+    modifier : Modifier = Modifier,
 ) {
-    Text("RecordScreen")
+    Column {
+        TopAppBar(
+            title = { Text(date) },
+            navigationIcon = {
+                IconButton(
+                    onClickDrawer
+                ) {
+                    Icon(imageVector = Icons.Rounded.Menu, contentDescription = stringResource(R.string.menu_content_description))
+                }
+            }
+        )
+    }
 }
 
 @Preview
