4c58a97875d9d00d3560042f662c2c907b071b89
이건희 <leech9876@naver.com>
2025-03-21 18:50:43 +0900

    #111 - style : spinner의 아이콘이 보이도록 설정

--- core/ui/src/main/java/hegunhee/routiner/ui/item/Spinner.kt
+++ core/ui/src/main/java/hegunhee/routiner/ui/item/Spinner.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import hegunhee.routiner.ui.R
@@ -33,7 +34,8 @@ fun Spinner(
         Icon(
             imageVector = Icons.Rounded.ArrowDropDown,
             contentDescription = stringResource(R.string.spinner_down_icon),
-            modifier = modifier.alignByBaseline()
+            modifier = modifier.alignByBaseline(),
+            tint = Color.White
         )
     }
 
@@ -56,7 +58,7 @@ fun Spinner(
 @Preview
 @Composable
 private fun SpinnerPreview() {
-    var items by remember { mutableStateOf(listOf("01", "02", "03")) }
+    val items by remember { mutableStateOf(listOf("01", "02", "03")) }
     var selectedItem by remember { mutableStateOf(items[0]) }
 
     val onSelectItemChanged: (String) -> Unit = { newItem ->
