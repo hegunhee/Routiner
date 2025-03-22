112cc6759026c14c297ef65f51d478e2a7557639
이건희 <leech9876@naver.com>
2025-03-22 11:14:13 +0900

    #118 - feat : 반복 루틴 추가 NavGraph 작성

--- feature/insertRoutine/src/main/java/com/hegunhee/routiner/insertRoutine/screen/navigation/InsertNavigation.kt
+++ feature/insertRoutine/src/main/java/com/hegunhee/routiner/insertRoutine/screen/navigation/InsertNavigation.kt
@@ -4,20 +4,31 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.hegunhee.routiner.insertRoutine.screen.InsertRoutineRootScreen
+import com.hegunhee.routiner.insertRoutine.screen.repeat.InsertRepeatRoutineScreen
 
 const val INSERT_ROUTINE_ROUTE = "INSERT_ROUTINE"
+
+const val INSERT_REPEAT_ROUTINE_ROUTE = "INSERT_REPEAT_ROUTINE"
 
 fun NavController.navigateInsertRoutine() {
     navigate(INSERT_ROUTINE_ROUTE)
 }
 
+fun NavController.navigateInsertRepeatRoutine() {
+    navigate(INSERT_REPEAT_ROUTINE_ROUTE)
+}
+
 fun NavGraphBuilder.insertNavGraph(
-    onClickBackButton : () -> Unit
+    onClickBackButton: () -> Unit
 ) {
     composable(INSERT_ROUTINE_ROUTE) {
         InsertRoutineRootScreen(
             onClickBackButton = onClickBackButton
         )
     }
+    composable(INSERT_REPEAT_ROUTINE_ROUTE) {
+        InsertRepeatRoutineScreen(
+            onClickBackButton = onClickBackButton
+        )
+    }
 }
-
