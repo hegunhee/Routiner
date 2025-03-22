db9c3c98fbd3178e4f24e8f1a45335c89ccd4c54
이건희 <leech9876@naver.com>
2025-03-22 10:26:46 +0900

    #114 - feat : 반복 루틴 화면 뼈대 작성

---
+++ feature/repeat/src/main/java/com/hegunhee/repeat/screen/RepeatRoutineRootScreen.kt
@@ -0,0 +1,54 @@
+package com.hegunhee.repeat.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.hegunhee.repeat.R
+import hegunhee.routiner.model.Date
+
+@Composable
+fun RepeatRoutineRootScreen(
+    onClickDrawer: () -> Unit,
+) {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RepeatRoutineScreen(
+    onClickDrawer: () -> Unit,
+) {
+    Column {
+        TopAppBar(
+            title = {
+                Text(stringResource(R.string.repeat_routine_desc))
+            },
+            navigationIcon = {
+                IconButton(
+                    onClickDrawer
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Menu,
+                        contentDescription = stringResource(R.string.menu_content_description)
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun RepeatRoutineScreenPreview() {
+    RepeatRoutineScreen(
+        onClickDrawer = {}
+    )
+}
