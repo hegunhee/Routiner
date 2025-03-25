daef6c7e8870bcd61f58e7701d10bfdda10a2ebe
이건희 <57277631+hegunhee@users.noreply.github.com>
2025-03-25 23:03:53 +0900

    Main 모듈의 View 관련 코드 제거 (#129)
    * #128 - remove : View기반 MainActivity, MainViewModel 삭제
    
    * #128 - style : composeMainActivity의 이름을 MainActivity로 변경
    
    * #128 - remove : guideDialog 관련 코드 삭제

--- feature/main/src/main/java/com/example/main/MainActivity.kt
+++ feature/main/src/main/java/com/example/main/MainActivity.kt
@@ -1,63 +1,21 @@
 package com.example.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.example.main.databinding.ActivityMainBinding
-import com.example.main.guide.GuideDialogFragment
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.main.app.RoutinerApp
 import dagger.hilt.android.AndroidEntryPoint
+import hegunhee.routiner.designsystem.theme.RoutinerTheme
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by viewModels()
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // FIXME : 다크모드 정식 오픈시 해당 코드 제거
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        binding = ActivityMainBinding.inflate(layoutInflater).apply {
-            this.viewmodel = viewModel
-        }
-        setContentView(binding.root)
-        if(viewModel.isAppFirstOpen() && !viewModel.isGuideDialogOpen()) {
-            openGuideDialog()
-        }
-        initActionBar()
-        initNavigation()
-    }
-
-    private fun initActionBar() = with(binding) {
-        setSupportActionBar(toolBar)
-        supportActionBar?.run {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_dehaze)
-        }
-    }
-
-    private fun initNavigation() = with(binding) {
-        (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).let {
-            val navController = it.navController
-            navView.setupWithNavController(navController)
-        }
-    }
-
-    private fun openGuideDialog() {
-        GuideDialogFragment.getInstance().show(supportFragmentManager,GuideDialogFragment.TAG)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                binding.drawerLayout.openDrawer(GravityCompat.START)
+        setContent {
+            RoutinerTheme {
+                RoutinerApp()
             }
         }
-        return super.onOptionsItemSelected(item)
     }
 }
