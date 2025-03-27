03d6b7dc7d34deb3cdd7dbf10942d4e8c287cc4e
이건희 <leech9876@naver.com>
2025-03-27 16:49:00 +0900

    #134 - style : screen 밑에있던 파일들을 repeat 패키지 하위로 이동

--- feature/repeat/src/main/java/com/hegunhee/repeat/navigation/RepeatRoutineNavGraph.kt
+++ feature/repeat/src/main/java/com/hegunhee/repeat/navigation/RepeatRoutineNavGraph.kt
@@ -1,8 +1,8 @@
-package com.hegunhee.repeat.screen.navigation
+package com.hegunhee.repeat.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.hegunhee.repeat.screen.RepeatRoutineRootScreen
+import com.hegunhee.repeat.RepeatRoutineRootScreen
 
 const val REPEAT_ROUTINE_ROUTE = "REPEAT_ROUTINE"
 
